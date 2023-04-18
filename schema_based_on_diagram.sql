cb959be89843d63c6a250ffc217c0b65ce4bd319
unknown <sohailshahidi1435@gmail.com>
2023-04-18 21:09:29 +0430

    Create new Tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -22,3 +22,27 @@ CREATE TABLE medical_histories (
     primary key (id),
 	foreign key (patient_id) REFERENCES patients (id)
 );
+
+CREATE TABLE medical_histories_treatments(
+	treatment_id int REFERENCES treatments (id),
+	medical_histories_id int REFERENCES medical_histories (id)	
+); 
+
+CREATE TABLE invoices (
+	id int GENERATED ALWAYS AS IDENTITY,
+	total_amount decimal,
+	generated_at TIMESTAMP NOT NULL DEFAULT NOW(),
+	payed_at TIMESTAMP,
+	medical_histories_id int UNIQUE REFERENCES medical_histories (id),
+	primary key (id)
+);
+
+CREATE TABLE invoice_items (
+	id int GENERATED ALWAYS AS IDENTITY,
+	unit_price decimal,
+	quantity int,
+	total_price decimal,
+	invoice_id int REFERENCES invoices (id),
+	treatment_id int REFERENCES treatments (id),
+	PRIMARY KEY (id)
+);
